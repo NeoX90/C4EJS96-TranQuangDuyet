0490351f056900014c1ceaa14c74bc5f5476095b
NeoX90 <hieuminhtran050300@gmail.com>
2020-09-15 16:18:39 +0700

    first commit

---
+++ hw1.js
@@ -0,0 +1,66 @@
+// b1
+// a)
+// ex:
+// + you can't use a number as ther first character
+// + you can't use any other characters, including spaces (khoa?ng trang), symbols and punctuation marks(da'u ca^u)
+// + you can't use one of JavaScript's reserved words (key words) as a variable name
+
+// b)In JavaScript, how to check a variable data types?
+
+// the answer:
+// use : console.log(typeof x) (x is variable name)
+// ex:
+// let x = 12345
+// console.log(typeof x);
+// => result is "number"
+
+// b2
+// 1:b     
+// 2:c      
+// 3:a
+// 4:e,d   
+
+// // b3
+// let message = ‘Coding is great’;
+// console.log(message);
+
+// var studentCount =0;
+// console.log(studentCount);
+
+// b4
+// a + c)
+// let message = "Coding might not be easy, but still great";
+// console.log(message);
+// message = message.toLowerCase();
+// console.log(message);
+
+// b + d)
+// var studentCount =16;
+// console.log(studentCount);
+// studentCount += 1;
+// console.log(studentCount);
+
+// b5
+// alert('you look beautiful today');
+
+// b6
+// var mess = prompt("hi, your name plaese?");
+// alert(mess);
+
+// b7
+// var fristName = prompt("enter your first name");
+// var lastName = prompt("enter your last name");
+// alert('hi '+ lastName+" "+ fristName)
+
+// b8
+// var x = prompt("enter sile lenght of square");
+// alert("the square area is "+ x*x)
+
+// b9
+// var x = prompt("enter radius of circle");
+// alert("the circle are is "+ parseFloat(Math.pow(x,2)*3.14))
+
+// b10
+// var x = prompt("enter the temperature in Celsius");
+// alert(x+"(C) = " + parseFloat(x*1.8 +32) + "(F)")
+
