2c6fcd518c3d22ed0030b0c6b5af19a28e8cec1b
NeoX90 <hieuminhtran050300@gmail.com>
2020-09-18 22:12:04 +0700

    update2

--- secssion2/hwork13/index.js
+++ secssion2/hwork13/index.js
@@ -2,6 +2,7 @@ let x =prompt("enter a number of edge: ");
 for(let i=1 ; i<=x ; i++){
     for( let j=1; j<= i+2 ;j++){
         fd(100);
-        rt(360/(i+2));
+        rt(360/(i+2)); 
+        
     }
 }
