1a0ff6d194c01f376803fd5dc7263e6f5e19c5b4
NeoX90 <hieuminhtran050300@gmail.com>
2020-09-19 21:19:11 +0700

    update session3

---
+++ session3/hw_video/index.js
@@ -0,0 +1,14 @@
+// ex: init
+let menuFilm = ['3 chu heo con','đố anh bắt được em'];
+console.log(menuFilm);
+
+// ex: create
+// let newItem = prompt('enter your favorite film')
+// menuFilm.push(newItem);
+// console.log(menuFilm)
+
+// ex: read
+// let i = prompt('enter a number between 0 to n');
+// console.log(menuFilm[i])
+
+// ex:update
