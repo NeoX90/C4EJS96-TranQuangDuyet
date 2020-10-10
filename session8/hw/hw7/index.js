b8af5a11ad62e6dcb48e7a80787caf4ff6797ee4
NeoX90 <hieuminhtran050300@gmail.com>
2020-10-10 11:19:05 +0700

    add

---
+++ session8/hw/hw7/index.js
@@ -0,0 +1,11 @@
+async function loadContent(){
+    const cont = await fetch(`http://quotes.rest/qod.json`) 
+    const data = await cont.json();
+    // console.log(data.contents.quotes[0].quote)  
+    let c = document.getElementById('content');
+    let a = document.getElementById('author');
+    // (data.contents.quotes[0].quote).innerHTML 
+    c.innerHTML = data.contents.quotes[0].quote;
+    a.innerHTML = data.contents.quotes[0].author
+} 
+loadContent();
