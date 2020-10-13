6e2555af522086b4d80e5888e24c5086fe853bf8
NeoX90 <hieuminhtran050300@gmail.com>
2020-10-13 08:17:42 +0700

    add data

--- basic shop/data.js
+++ basic shop/data.js
@@ -103,4 +103,341 @@ const products =[
         price : 400,
         productType : `jacket`,
     }, 
+    // vietkhuat data
+    {   
+        category : `accessories`,
+        name : `GOTHIC EMBROIDERED HAT`,
+        brand : `VETEMENTS`,
+        size : [`no size`],
+        color : ['black'],
+        gender : ['male','female'],
+        price : 350,
+        productType : `hats`,
+            // t-shirt, pants , suits ,trousers ,...
+    },
+    {   
+        category : `accessories`,
+        name : `LOGO MERINO-WOOL SCARF`,
+        brand : `VETEMENTS`,
+        size : [`no size`],
+        color : ['white','black'],
+        gender : ['female'],
+        price : 750,
+        productType : `scarves`,
+    },
+    {   
+        category : `apparel`,
+        name : `LOGO PRINT RAINCOAT`,
+        brand : `VETEMENTS`,
+        size : ['S','M','L','XL','XXL'],
+        color : ['black'],
+        gender : ['male'],
+        price : 1350,
+        productType : `coast`,
+    },
+    {   
+        category : `shoes,`,
+        name : `SPIKE RUNNER 2 SNEAKERS`,
+        brand : `VETEMENTS`,
+        size : [7,7.5,8,8.5,9,9.5,10,10.5,11,11.5,12],
+        color : ['orange','red','blue'],
+        gender : ['male','female'],
+        price : 590,
+        productType : [`shoes`, `sneakers`],
+    },
+    {   
+        category : `shoes`,
+        name : `BOOMERANG PATENT LEATHER ANKLE BOOTS`,
+        brand : `VETEMENTS`,
+        size : [7,7.5,8,8.5,9,9.5,10,10.5,11,11.5,12],
+        color : ['black',],
+        gender : ['female'],
+        price : 1390,
+        productType : [`shoes`,`boots`],
+    },
+    {   
+        category : `apparel`,
+        name : `REVERSIBLE WHITE & BLACK BOMBER JACKET`,
+        brand : `VETEMENTS`,
+        size : ['S','M','L','XL','XXL'],
+        color : ['black','white'],
+        gender : ['women'],
+        price : 1590,
+        productType : `jackets`,
+    },
+    {   
+        category : `apparel`,
+        name : `CRYSTAL-EMBELLISHED HOODED SWEATSHIRT IN BLACK`,
+        brand : `VETEMENTS`,
+        size : ['S','M','L','XL','XXL'],
+        color : ['black'],
+        gender : ['women'],
+        price : 770,
+        productType : `hoodie`,
+    },
+    {   
+        category : `accessories`,
+        name : `BLACK LOGO BACKPACK`,
+        brand : `VETEMENTS`,
+        size : [],
+        color : ['black',],
+        gender : ['unisex'],
+        price : 850,
+        productType : `bags`,
+    },
+    {   
+        category : `apparel`,
+        name : `IT COSTS COTTON T-SHIRT`,
+        brand : `VETEMENTS`,
+        size : ['S','M','L','XL','XXL'],
+        color : ['black'],
+        gender : ['unisex'],
+        price : 190,
+        productType : `t-shirts`,
+    },
+    {   
+        category : `accessories`,
+        name : `LOGO-ENGRAVED SIGNET RING`,
+        brand : `VETEMENTS`,
+        size : ['12','13','14','15'],
+        color : ['silver'],
+        gender : ['unisex'],
+        material : ['brass'],
+        price : 300,
+        productType : `ring`,
+    }, 
+    //ng bao long data
+    {
+        category : `apparel`,
+                // + shoes 
+                // + accessories
+        name : `WE ARE PINK MEDIUM FIT HOODIE`,
+        brand : `balenciaga`,
+        size : ['S','M','L','XL','XXL'],
+        color : ['black','while'],
+        gender : ['male'],
+                // female
+        price : 950,
+        productType : `hoodie`,
+    },
+    {
+        category : `apparel`,
+                // + shoes 
+                // + accessories
+        name : `WE ARE PINK LARGE FIT T-SHIRT`,
+        brand : `balenciaga`,
+        size : ['S','M','L','XL','XXL'],
+        color : ['black','while'],
+        gender : ['male'],
+                // female
+        price : 510,
+        productType : `t-shirt`,
+    },
+    {
+        category : `accessories`,
+                // + shoes 
+                // + apparel
+        name : `NEO CLASSIC LARGE TOP HANDLE BAG`,
+        brand : `balenciaga`,
+        size : [`no size`],
+        color : ['black','while'],
+        gender : ['female'],
+                
+        price : 2650,
+        productType : `bag`,
+    },
+    {
+        category : `accessories`,
+                // + shoes 
+                // + apparel
+        name : `NEO CLASSIC SMALL TOP HANDLE BAG`,
+        brand : `balenciaga`,
+        size : [`no size`],
+        color : ['black','grey'],
+        gender : ['female'],
+                
+        price : 2250,
+        productType : `bag`,
+    },
+    {
+        category : `accessories`,
+                // + shoes 
+                // + apparel
+        name : `NEO CLASSIC MINI TOP HANDLE BAG`,
+        brand : `balenciaga`,
+        size : [`no size`],
+        color : ['black','blue','red'],
+        gender : ['female'],
+                
+        price : 1980,
+        productType : `bag`,
+    },
+    {
+        category : `shoes`,
+                // + accessories 
+                // + apparel
+        name : `TRACK SLIDE SANDAL`,
+        brand : `balenciaga`,
+        size : [4,4.5,5,5.5,6,6.5,7,7.5,8],
+        color : ['black','while'],
+        gender : ['female'],
+                //male
+        price : 630,
+        productType : `track`,
+    },
+    {
+        category : `shoes`,
+                // + accessories 
+                // + apparel
+        name : `TRACK SNEAKER`,
+        brand : `balenciaga`,
+        size : [4 , 4.5 , 5 , 5.5 , 6 , 6.5 , 7 , 7.5 , 8 , 8.5 , 9],
+        color : ['black','while','blue',],
+        gender : ['female'],
+                //male
+        price : 1005,
+        productType : `track`,
+    },
+    {
+        category : `shoes`,
+                // + accessories 
+                // + apparel
+        name : `TRIPLE S SNEAKER`,
+        brand : `balenciaga`,
+        size : [4 , 4.5 , 5 , 5.5 , 6 , 6.5 , 7 , 7.5 , 8],
+        color : ['black','while','red','blue','grey'],
+        gender : ['female'],
+                //male
+        price : 1110,
+        productType : `triple s`,
+    },
+    {
+        category : `shoes`,
+                // + accessories 
+                // + apparel
+        name : `SPEED SNEAKER`,
+        brand : `balenciaga`,
+        size : [4 , 4.5 , 5 , 5.5 , 6 , 6.5 , 7 , 7.5 , 8],
+        color : ['black','while','muticolor'],
+        gender : ['female'],
+                //male
+        price : 840,
+        productType : `speed`,
+    },
+    {
+        category : `shoes`,
+                // + accessories 
+                // + apparel
+        name : `SPEED 2.0 SNEAKER`,
+        brand : `balenciaga`,
+        size : [4 , 4.5 , 5 , 5.5 , 6 , 6.5 , 7 , 7.5 , 8],
+        color : ['black','while','red','blue','grey','muticolor'],
+        gender : ['female'],
+                //male
+        price : 950,
+        productType : `speed 2.0`,
+    },
+    // do duy hieu data
+    {   
+        category : `apparel`,
+        name : `Nike Sportwear`,
+        brand : `Nike`,
+        size : ['S','M','L','XL','XXL'],
+        color : ['white','black','grey'],
+        gender : ['male','female'],
+                 
+        price : 48,
+        productType : `T- Shirt`,
+           
+    },
+    {   
+        category : `apparel`,
+        name : `Nike Dri-FIT Player`,
+        brand : `Nike`,
+        size : ['S','M','L','XL','XXL'],
+        color : ['white','black','grey',],
+        gender : ['male'],
+        price : 150,
+        productType : `Polo`,
+    },
+    {   
+        category : `apparel`,
+        name : `Short-Sleeve Running Top`,
+        brand : `Nike`,
+        size : ['S','M','L','XL'],
+        color : ['white','black','grey'],
+        gender : ['famale'],
+        price : 220,
+        productType : `T-shirt`,
+    },
+    {   
+        category : `shoes`,
+        name : `Nike Air Force 1 Crater`,
+        brand : `Nike`,
+        size : [7,7.5,8,8.5,9,9.5,10,10.5,11,11.5,12],
+        color : ['black','pink','blue','white'],
+        gender : ['male','female'],
+        price : 1050,
+        productType : `originals`,
+    },
+    {   
+        category : `shoes`,
+        name : `Nike Air Force 1 React LV8`,
+        brand : `Nike`,
+        size : [7,7.5,8,8.5,9,9.5,10,10.5,11,11.5,12],
+        color : ['cyan','yellow','black','white'],
+        gender : ['male','female'],
+        price : 900,
+        productType : `originals`,
+    },
+    {   
+        category : `shoes`,
+        name : `Nike Tạnun`,
+        brand : `Nike`,
+        size : [7,7.5,8,8.5,9,9.5,10,10.5,11,11.5,12],
+        color : ['cyan','yellow','black','white'],
+        gender : ['male'],
+        price : 1500,
+        productType : `running`,
+    },
+    {   
+        category : `apparel`,
+        name : `Nike BETRUE`,
+        brand : `Nike`,
+        size : ['S','M','L','XL','XXL'],
+        color : ['white','black'],
+        gender : ['male'],
+        price : 780,
+        productType : `hoodie`,
+    },
+    {   
+        category : `apparel`,
+        name : `Nike Sport Tech Pack`,
+        brand : `Nike`,
+        size : ['S','M','L','XL','XXL'],
+        color : ['white','black','blue'],
+        gender : ['male','female'],
+        price : 468,
+        productType : `Jacket`,
+    },
+    {   
+        category : `apparel`,
+        name : `Nike SportSwear`,
+        brand : `Nike`,
+        size : ['S','M','L','XL'],
+        color : ['Track Rede'],
+        gender : ['female'],
+        price : 860,
+        productType : `Dress`,
+    },
+    {   
+        category : `apparel`,
+        name : `Nike Essential`,
+        brand : `Nike`,
+        size : ['S','M','L','XL','XXL'],
+        color : ['white','black','grey'],
+        gender : ['male'],
+        price : 400,
+        productType : `pants `,
+    }, 
 ]
