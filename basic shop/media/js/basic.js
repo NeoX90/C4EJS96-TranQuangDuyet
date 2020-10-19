cbcbacbcfbacc092b40a8ebc56afebe27ad05109
NeoX90 <hieuminhtran050300@gmail.com>
2020-10-19 13:24:29 +0700

    add

--- basic shop/media/js/basic.js
+++ basic shop/media/js/basic.js
@@ -1,1 +1,23 @@
-console.log(products[0].img[1])
+
+let mainPro= document.getElementById("showList");
+function soProduct(img, name, description, price)
+{
+    mainPro.insertAdjacentHTML('beforeEnd', 
+    `
+    <div class="productItem">
+      <div class="productImg"><img src="${img}" alt="" style = "display :flex"></div>
+      <div class="productTit">
+        <b>${name}</b> <br>
+        <span>${description}</span>
+      </div>
+      <div class="productPrice">${price}</div>
+      <button class="addPro">Add to cart</button>
+     </div>
+    `
+    )
+}
+mainPro.innerHTML = '';
+for(let i=0; i< products.length; i++)
+{
+    soProduct(products[i].img[0], products[i].name, products[i].description, products[i].price);
+}
