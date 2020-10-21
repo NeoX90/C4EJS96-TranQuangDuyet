8c17c2e62ce93a1c837cb2d68fa4b630d0411a91
NeoX90 <hieuminhtran050300@gmail.com>
2020-10-21 21:48:59 +0700

    add search

--- basic shop/media/js/basic.js
+++ basic shop/media/js/basic.js
@@ -1,10 +1,10 @@
 let showNike = document.getElementById('nike');
-let mainPro= document.getElementById("showList");
+let showList= document.getElementById("showList");
 let search_Text = document.getElementById("search");
 let search_btn = document.getElementById("search-btn");
 function soProduct(img, name, description, price)
 {
-    mainPro.insertAdjacentHTML('beforeEnd', 
+    showList.insertAdjacentHTML('beforeEnd', 
     `
     <div class="productItem">
       <div class="productImg"><img src="${img}" alt="" style = "display :flex"></div>
@@ -18,46 +18,61 @@ function soProduct(img, name, description, price)
     `
     )
 }
-// mainPro.innerHTML = '';
-// for(let i=0; i< products.length; i++)
-// {
-    // soProduct(products[i].img[0], products[i].name, products[i].description, products[i].price);
-// }
 
-// console.log(products.length)
-// function searchNike(){
-//   for(let i = 0 ; i < products.length ; i++){
-//     if(products[i].brand == 'Nike'){
-//       soProduct(products[i].img[0], products[i].name, products[i].description, products[i].price);
-//     }
-//   }
-// }
+function searchResult (name){
+  filter-ul.insertAdjacentElement('beforeEnd',
+  `
+    <div>
+      <ul id="filter-ul">
+        <li class="filter">${name}</li>
+      </ul>
+    </div>
+    `
+  )
+}
+
+showList.innerHTML = '';
+for(let i=0; i< products.length; i++)
+{
+    soProduct(products[i].img[0], products[i].name, products[i].description, products[i].price);
+}
 
 // tìm kiếm theo brand
 showNike.addEventListener('click',()=>{
+  showList.innerHTML = '';
   for(let i = 0 ; i < products.length ; i++){
     if(products[i].brand == 'Nike'){
       soProduct(products[i].img[0], products[i].name, products[i].description, products[i].price);
+      
     }
   }
 })
-
 // tìm kiếm theo tên
-search_btn.addEventListener('click',()=>{
-
-})
-
 function getInputValue(){
   // Selecting the input element and get its value 
-  var inputVal = search_Text.value;
+  showList.innerHTML ='';
+  var inputVal = search_Text.value.toLowerCase();
   for(let i = 0 ; i < products.length ;i++){
-    let x = products[i].name.split(' ')
-    for(let j = 0 ; j < x.length ; j++){
-      if(x[j] == inputVal){
+    let x = products[i].name.toLowerCase()
+      if(x == inputVal){
         soProduct(products[i].img[0], products[i].name, products[i].description, products[i].price);
+        searchResult(products[i].name);
+    }
+  }
+}
+ // tìm kiếm search live
+function liveSearch(){
+  let fillterUl = document.getElementById('filter-ul');
+  fillterUl.innerHTML = "  ";
+  if (search_Text.value.length > 0) {
+    for(let i =0; i<products.length ; i++){
+      a = products[i].name;
+      if(a.toUpperCase().indexOf((search_Text.value.toUpperCase())) > -1){
+        fillterUl.innerHTML += `<li style = "display : block;">${a}</li>`
+      }
+      else {
+        fillterUl.innerHTML += `<li style = "display : none;">${a}</li>`
       }
     }
   }
-  // Displaying the value
-  // alert(inputVal);
 }
